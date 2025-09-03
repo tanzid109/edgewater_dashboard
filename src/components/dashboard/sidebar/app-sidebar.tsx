10b1e7ec2b70168079ce132205931c16a022f9d0
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-09-03 06:16:48 +0600

    Dashboard init

---
+++ src/components/dashboard/sidebar/app-sidebar.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import * as React from "react"
+import {
+    AudioWaveform,
+    Command,
+    GalleryVerticalEnd,
+    UserRoundCogIcon
+} from "lucide-react"
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { RiBarChartBoxLine } from "react-icons/ri";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarRail,
+} from "@/components/ui/sidebar"
+import { TeamSwitcher } from "./team-switcher"
+import { NavMain } from "./nav-main"
+import { NavUser } from "./nav-user"
+
+// This is sample data.
+const data = {
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+    },
+    teams: [
+        {
+            name: "Acme Inc",
+            logo: GalleryVerticalEnd,
+            plan: "Enterprise",
+        },
+        {
+            name: "Acme Corp.",
+            logo: AudioWaveform,
+            plan: "Startup",
+        },
+        {
+            name: "Evil Corp.",
+            logo: Command,
+            plan: "Free",
+        },
+    ],
+    navMain: [
+        {
+            title: "Dashboard",
+            url: "#",
+            icon: TbLayoutDashboardFilled ,
+            isActive: true,
+        },
+        {
+            title: "User Management",
+            url: "#",
+            icon: UserRoundCogIcon,
+        },
+        {
+            title: "CRSS Data Report",
+            url: "#",
+            icon: RiBarChartBoxLine,
+        },
+    ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <TeamSwitcher teams={data.teams} />
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={data.user} />
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    )
+}
---
+++ src/components/dashboard/sidebar/nav-main.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import {
+    Collapsible,
+    CollapsibleContent,
+} from "@/components/ui/collapsible"
+import {
+    SidebarGroup,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+import { LucideIcon } from "lucide-react"
+
+export function NavMain({
+    items,
+}: {
+    items: {
+        title: string
+        url: string
+        icon?:  LucideIcon | undefined 
+        isActive?: boolean
+        items?: {
+            title: string
+            url: string
+        }[]
+    }[]
+}) {
+    return (
+        <SidebarGroup>
+            <SidebarMenu>
+                {items.map((item) => (
+                    <Collapsible
+                        key={item.title}
+                        asChild
+                        defaultOpen={item.isActive}
+                    >
+                        <SidebarMenuItem>
+                                <SidebarMenuButton tooltip={item.title}>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </SidebarMenuButton>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    {item.items?.map((subItem) => (
+                                        <SidebarMenuSubItem key={subItem.title}>
+                                            <SidebarMenuSubButton asChild>
+                                                <a href={subItem.url}>
+                                                    <span>{subItem.title}</span>
+                                                </a>
+                                            </SidebarMenuSubButton>
+                                        </SidebarMenuSubItem>
+                                    ))}
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    )
+}
