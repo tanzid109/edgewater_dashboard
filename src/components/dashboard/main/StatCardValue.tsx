17ea5df66c478cde24ccd280bb6f29e1305fe726
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-09-22 23:40:44 +0600

    stat changes

--- src/components/dashboard/main/StatCardValue.tsx
+++ src/components/dashboard/main/StatCardValue.tsx
@@ -4,9 +4,9 @@ export default function StatCardValue() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             <StatCard image="/icons/clipboard.svg" value={768} label="Total Form Submitted" />
-            <StatCard image="/icons/restore.svg" value={186} label="Total Repeat Visitors" />
-            <StatCard image="/icons/user.svg" value={125} label="Total Follow up Visits" />
-            <StatCard image="/icons/money.svg" value="$360" label="Total Cost of Care" />
+            <StatCard image="/icons/siren.svg" value={186} label="Crisis Calls" />
+            <StatCard image="/icons/mobile.svg" value={125} label="Mobile Crisis" />
+            <StatCard image="/icons/stabilization.svg" value="$360" label="Crisis Stabilization Unit" />
         </div>
     )
 }
